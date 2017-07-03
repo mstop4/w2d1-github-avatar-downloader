206a64cde264c1088e38d6a53ca128b14ffcd2b9
mstop4 <mstop4@gmail.com>
2017-07-03 20:12:16 +0000

    downloads avatars

--- download_avatars.js
+++ download_avatars.js
@@ -5,7 +5,8 @@
  *---------------------*/
 
 // Init packages
-var request = require('request');
+var request = require("request");
+var fs = require("fs");
 
 // GitHub info
 var GITHUB_USER = "mstop4";
@@ -13,25 +14,50 @@ var GITHUB_TOKEN = "ae8066a4e51b2e538d0b9f636b46f1b60f3f5b1e";
 var repoOwner = "mstop4";
 var repoName = "FMODGMS";
 
-function parseContributors(contributorList) {
+var avatarDir = "./avatars"
 
-  //Parse string -> JSON
-  contrib_JSON = JSON.parse(contributorList);
+function dlComplete(message) {
+  console.log("Avatar downloaded to: " + message);
+}
 
-  for (var i = 0; i < contrib_JSON.length; i++ ) {
-    console.log(contrib_JSON[i]["avatar_url"]);
+function createDir(dir) {
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
   }
 }
 
+function downloadImageByURL(url, filePath) {
 
-// Start
-console.log("Welcome to the GitHub Avatar Downloader!");
+  request.get(url)
+         .on("error", function (err) {
+            console.log("Error downloading image!");
+            throw err;
+         })
+         .on("response", function (response) {
+            //console.log("Response Status Code: ", response.statusCode);
+            //console.log("Content Type: ", response.headers["content-type"]);
+            console.log("Downloading avatar from " + url + " ...");
+         })
+         .pipe(fs.createWriteStream(filePath)
+                 .on("finish", function () { dlComplete(filePath) }));    // show message when download is complete
+}
+
+function parseContributors(contributorList) {
+
+  //Parse string -> JSON
+  var contrib_JSON = JSON.parse(contributorList);
+
+  for (var i = 0; i < contrib_JSON.length; i++ ) {
+    //console.log(contrib_JSON[i]['login']);
+    downloadImageByURL(contrib_JSON[i]["avatar_url"] ,avatarDir + "/" + contrib_JSON[i]["login"] + ".png");
+  }
+}
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://" + GITHUB_USER + ":" + GITHUB_TOKEN + "@api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+      "User-Agent": "GitHub Avatar Downloader - Student Project"
     }
   };
 
@@ -53,4 +79,8 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
+// Start
+console.log("Welcome to the GitHub Avatar Downloader!");
+
+createDir(avatarDir);
 getRepoContributors(repoOwner, repoName, parseContributors);
