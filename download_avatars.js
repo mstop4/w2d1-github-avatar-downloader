19b8b098a0b207d400af5266f0f3b038579086dd
mstop4 <mstop4@gmail.com>
2017-07-03 18:51:35 +0000

    added github info

--- download_avatars.js
+++ download_avatars.js
@@ -4,7 +4,17 @@
  *                     *
  *---------------------*/
 
+// Init packages
 var request = require('request');
+
+// GitHub info
+var GITHUB_USER = "mstop4";
+var GITHUB_TOKEN = "ae8066a4e51b2e538d0b9f636b46f1b60f3f5b1e";
+var repoOwner = "mstop4";
+var repoName = "FMODGMS";
+var requestURL = "https://" + GITHUB_USER + ":" + GITHUB_TOKEN + "@api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+
+console.log(requestURL);
 
 console.log("Welcome to the GitHub Avatar Downloader!");
 
@@ -12,7 +22,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 }
 
-getRepoContributors("mstop4", "FMODGMS", function(err, result) {
+getRepoContributors(repoOwner, repoName, function(err, result) {
   console.log("Errors: ", err);
   console.log("Result: ", result);
 })
