fd85b5fa7c84fd7282df0bfd6e016d53f3f6fd26
mstop4 <mstop4@gmail.com>
2017-07-03 18:45:39 +0000

    started getRepoContributors

--- download_avatars.js
+++ download_avatars.js
@@ -7,3 +7,12 @@
 var request = require('request');
 
 console.log("Welcome to the GitHub Avatar Downloader!");
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+}
+
+getRepoContributors("mstop4", "FMODGMS", function(err, result) {
+  console.log("Errors: ", err);
+  console.log("Result: ", result);
+})
